ec8e83a68468de35272d64d730883e103c279031
Gustavo Cunha <gusxpander@gmail.com>
2020-05-31 23:30:11 -0300

    create a nav logic

--- movements_cordenator.py
+++ movements_cordenator.py
@@ -1,11 +1,54 @@
 import core
 
-horizontal_postion = ''
-vertical_position = ''
-side = ''
 
-def imput_receiver(moviment):
+class Probe():
 
-    postion = {}
+    horizontal_postion = 0
+    vertical_position = 0
+    side = 'N'
 
-    return postion
+    def __move_forward(self, moviment):
+
+        if self.side == 'N':
+            self.horizontal_postion += 1
+        elif self.side == 'W':
+            self.vertical_position += 1
+        elif self.side == "S":
+            self.horizontal_postion -= 1
+        elif self.side == 'E':
+            self.vertical_position -= 1
+
+    def __rote(self, command):
+
+        moviments = {
+            "N": {"L": "W", "R": "E"},
+            "W": {"L": "S", "R": "N"},
+            "S": {"L": "E", "R": "W"},
+            "E": {"L": "N", "R": "S"}
+        }
+
+        self.side = moviments.get(self.side).get(command, "Wrong moviment")
+
+    def imput_receiver(self, command):
+        if command == "M":
+            self.__move_forward(command)
+        elif command == "L" or command == "R":
+            self.__rote(command)
+
+        postion = {
+            'horizontal_postion': self.horizontal_postion,
+            'vertical_position': self.vertical_position,
+            'side': self.side
+        }
+
+        return postion
+
+
+if __name__ == "__main__":
+
+    side = ""
+    probe = Probe()
+
+    while side != "exit":
+        side = input("moviment? ")
+        print(probe.imput_receiver(side))
