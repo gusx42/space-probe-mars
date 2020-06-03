f6bdbe8ed6a173295ce4b4e0c4b4a896e19398cb
Gustavo Cunha <gusxpander@gmail.com>
2020-06-03 00:47:45 -0300

    ajusta navigation

--- movements_cordenator.py
+++ movements_cordenator.py
@@ -6,17 +6,17 @@ class Probe():
     horizontal_postion = 0
     vertical_position = 0
     side = 'N'
-
+    
     def __move_forward(self, moviment):
 
         if self.side == 'N':
-            self.horizontal_postion += 1
-        elif self.side == 'W':
             self.vertical_position += 1
+        elif self.side == 'W':
+            self.horizontal_postion += 1
         elif self.side == "S":
-            self.horizontal_postion -= 1
-        elif self.side == 'E':
             self.vertical_position -= 1
+        elif self.side == 'E':
+            self.horizontal_postion -= 1
 
     def __rote(self, command):
 
@@ -29,19 +29,25 @@ class Probe():
 
         self.side = moviments.get(self.side).get(command, "Wrong moviment")
 
-    def imput_receiver(self, command):
-        if command == "M":
-            self.__move_forward(command)
-        elif command == "L" or command == "R":
-            self.__rote(command)
+    def imput_receiver(self, commands):
 
-        postion = {
-            'horizontal_postion': self.horizontal_postion,
-            'vertical_position': self.vertical_position,
-            'side': self.side
-        }
 
-        return postion
+        for command in commands:
+
+            if command == "M":
+                self.__move_forward(command)
+            elif command == "L" or command == "R":
+                self.__rote(command)
+
+            postion = {
+                'h': self.horizontal_postion,
+                'v': self.vertical_position,
+                'side': self.side
+            }
+
+            print(postion)
+
+        
 
 
 if __name__ == "__main__":
@@ -51,4 +57,4 @@ if __name__ == "__main__":
 
     while side != "exit":
         side = input("moviment? ")
-        print(probe.imput_receiver(side))
+        probe.imput_receiver(side)
